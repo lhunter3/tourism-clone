30623ae510a5e3b50294795866a2178e44b5fcfb
Lucas Hunter <lucasbhunter@gmail.com>
2024-06-13 22:16:02 -0300

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import React, {lazy} from 'react';
+import React from 'react';
 import './App.css';
 
 import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 
-const Home = lazy(() => import('./Pages/LandingPage'));
-const Packages = lazy(() => import('./Pages/Packages'));
+import { Home } from './Pages/LandingPage';
+import { Packages } from './Pages/Packages';
 import { Suspense } from 'react';
 
-const Loader = () => console.log("preloading"); // Replace this with your actual preloader
 
 export default function App() {
 
@@ -15,12 +14,10 @@ export default function App() {
   return (
 
     <Router>
-      <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path='/' element={Home} />
-        <Route path='/packages' element={Packages} />
+        <Route path='/' element={<Home />} />
+        <Route path='/packages' element={<Packages />} />
       </Routes>
-      </Suspense>
     </Router>
 
   );
