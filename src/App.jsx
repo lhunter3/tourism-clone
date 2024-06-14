1d9229c30479d36bae9e8aae9b221a42eec3ee35
Lucas Hunter <lucasbhunter@gmail.com>
2024-06-13 22:08:27 -0300

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css';
 
 import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 
-import { Home } from './Pages/LandingPage';
-import { Packages } from './Pages/Packages';
+const Home = lazy(() => import('./Pages/LandingPage'));
+const Packages = lazy(() => import('./Pages/Packages'));
 import { Suspense } from 'react';
 
 const Loader = () => console.log("preloading"); // Replace this with your actual preloader
@@ -17,8 +17,8 @@ export default function App() {
     <Router>
       <Suspense fallback={<Loader />}>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/packages' element={<Packages />} />
+        <Route path='/' element={Home} />
+        <Route path='/packages' element={Packages} />
       </Routes>
       </Suspense>
     </Router>
